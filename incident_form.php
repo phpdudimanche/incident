1a0f35669f424e0c590e50a26c70f00c71642de4
phpdudimanche <julien.gontier@gmail.com>
2014-12-13 16:45:54 +0000

    version 00.00.01

---
+++ incident_form.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL); 
+
+isset($_REQUEST['act'])?$act=$_REQUEST['act']:$act='';
+isset($_REQUEST['id'])?$id=$_REQUEST['id']:$id='';
+$title='Rapport d\'incident';
+
+require_once '_haut.php';
+
+require_once 'incident.php';
+$incident = new incident;
+
+print("
+<h1>$title</h1>
+");
+
+
+if($act=="create"){
+$form_creation=$incident->display_crea_incident();
+echo $form_creation;
+}
+elseif($act=="update"){
+ // query select fetch  
+$result=$incident->retrieve_id_incident($con,$id);
+//print_r($result);
+$id=$result[0]['id'];
+$resume=$result[0]['resume'];
+$severite=$result[0]['severite'];
+$urgence=$result[0]['urgence'];
+$description=$result[0]['description'];
+$form_modification=$incident->display_modif_incident($id,$resume,$severite,$urgence,$description);
+echo $form_modification;
+}
+else{
+    echo "que faire ?";//@todo par défaut proposer la création ?
+}
+
+require_once '_bas.php';
+?>
---
+++ incident_list.php
@@ -0,0 +1,44 @@
+<?php
+//ini_set('display_errors', 1); 
+//error_reporting(E_ALL); 
+isset($_REQUEST['act'])?$act=$_REQUEST['act']:$act='';
+isset($_REQUEST['id'])?$id=$_REQUEST['id']:$id='';
+
+$title='Affichage d\'incident';
+
+    require_once '_haut.php';
+    require_once 'incident.php';
+    $incident = new incident;
+
+print("
+<h1>$title</h1>
+");
+
+if ($act=='list'){// liste de N incidents
+$result=$incident->retrieve_n_incident($con);
+        if($debug===1){
+            echo '<br/>debug:'.$debug;
+        echo '<pre>';
+        print_r($result);
+        echo '</pre>';
+        }
+$incident->display_admin_n_incident($result);
+}
+elseif($act=='view'){// vue non modifiable sur 1 incident imprimable
+    $result=$incident->retrieve_id_incident($con,$id);
+    //echo $result;
+    if ($result=='rien'){
+    echo 'Aucun incident avec cet identifiant.';
+    }
+    else{
+    //print_r($result);
+    $incident->display_vue_incident($result);
+    }
+}
+else{
+   // echo "rien de prévu ?";
+$result=$incident->retrieve_n_incident($con);
+$incident->display_admin_n_incident($result);
+}
+    require_once '_bas.php';
+?>
