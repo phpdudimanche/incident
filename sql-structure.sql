77da9381467e17376d8f094c353dc7b6aa11641c
phpdudimanche <julien.gontier@gmail.com>
2014-12-13 21:36:20 +0000

    version 00.00.01

--- sql-structure.sql
+++ sql-structure.sql
@@ -1,5 +1,5 @@
 # tables de la base gestionnaire incident sans ? autoincrement pour portabilite
-CREATE table `c9`.`incident` (
+CREATE table `incident` (
 `id` INT(5) PRIMARY KEY NOT NULL auto_increment ,
 `resume` VARCHAR(250),
 `description` TEXT,
